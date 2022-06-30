c99048ad77c778db07dd0ef84d57ef4e31188042
collins <kevin1520smart@gmail.com>
2022-06-30 17:13:57 +0100

    intro2,intro3,signup...all in progress

--- src/Intro.js
+++ src/Intro.js
@@ -1,14 +1,14 @@
-import './App.css';
-import { useState, useEffect } from "react";
-// import Intro2 from "./Intro2"
-// import Intro3 from "./Intro3"
-// import SignUp from "./SignUp"
-
+import './App.css'
+import { useState } from "react"
+import Intro2 from "./Intro2"
+import Intro3 from "./Intro3"
+import SignUp from "./SignUp"
+import {Circle} from "phosphor-react"
 
 function Intro(){
 	
-	const [nextPhase,setNextPhase] = useState(false);
-	const [signUp,setSignUp] = useState(false);
+	const [nextPhase,setNextPhase] = useState(false)
+	const [signUp,setSignUp] = useState(false)
 	function onNext(){
 		setNextPhase(true)
 	}
@@ -20,24 +20,24 @@ function Intro(){
 	return(
 			<div className="intro">
 				{
-					// nextPhase ? 
-					// 	<Intro2/> :
-					// signUp ? 
-					// 	<SignUp/> :
+					nextPhase ? 
+						<Intro2/> :
+					signUp ? 
+						<SignUp/> :
 					<div>
 						<div className="intro-main">
 							<div  className="intro-img"></div>
 						</div>
 						<h2>Donate easily, quickly, right on target all over the world</h2>
 						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
-						<div>
-						  <span class="on"></span> 
-						  <span class="dot"></span> 
-						  <span class="dot"></span> 
+						<div className="circle">
+						  <Circle size={10} color="#1EBA60" weight="fill" />
+						  <Circle size={10} color="#EBEEF2" weight="duotone" />
+						  <Circle size={10} color="#EBEEF2" weight="duotone" />
 						</div>
-						<div className="btn">
-							<button onClick={setSignUp}>Skip</button>
-							<button onClick={setNextPhase}>Next</button>
+						<div className="end">
+							<button className="nxt" onClick={setNextPhase}>Next</button>
+							<button className="nxt" onClick={setSignUp}>Skip</button>
 						</div>
 					</div>
 				}
