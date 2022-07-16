706b3a0c8639c46085a0eba9d0c0c7c72300612f
collins <kevin1520smart@gmail.com>
2022-07-16 14:57:04 +0100

    created bookmark items

--- src/Bookmark.jsx
+++ src/Bookmark.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import {useState} from "react"
 import Menu from "./Menu"
+import BookMarkItem from "./BookMarkItem"
 // import Remove from "./Remove"
 import Homepage from "./Homepage"
 import {DotsThreeVertical,BookmarkSimple,ArrowLeft} from "phosphor-react"
@@ -43,29 +44,16 @@ function Bookmark(){
                         <button>Music</button>
                         <button>Sport</button>
                     </div>
-                    <div className="bookmark-card">
-                        <div>
-                            <div className="bookmark-img">
-                                <div className="bookmark-mark" onClick={onBookMark}>
-                                    <BookmarkSimple size={20} color="#1EBA60" weight="fill" />
-                                </div>
-                            </div>
-                            <div>
-                                <div className="bookmark-note">
-                                    <h3>Help Dilan for brain Turmor Surgery</h3>
-                                    <p>
-                                        <span>$7,379 </span>
-                                        fund raised from $8,200
-                                    </p>
-                                </div>
-                                <div className="bookmark-bar"></div>
-                                <div className="bookmark-end">
-                                    <p><span>3,274</span> Donators</p>
-                                    <p><span>12</span> days left</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <BookMarkItem
+                        img= "bookmark-img"
+                        title= "Help Dilan Brain Turmor Surgery"
+                        amt= "$7,379"
+                        txt= "fund rasied from $8,200"
+                        num= "3,274"
+                        info= "Donators"
+                        num2= "12"
+                        info2= "days left"
+                    />
                     <div className="bookmark-card">
                         <div>
                             <div className="bookmark-img2">
