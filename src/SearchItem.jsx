617ab35953891ec22035883c39a9efca6a4791db
collins <kevin1520smart@gmail.com>
2022-07-18 13:07:09 +0100

    styled the searchItem section

--- src/SearchItem.jsx
+++ src/SearchItem.jsx
@@ -5,22 +5,17 @@ function SearchItem({img,title,amt,info,txt,num,txt2,num2}){
     return(
         <div className="search-result">
             <div className={img}>
-                <div className="urgent-mark">
+                <div className="bookmark-mark">
                     <BookmarkSimple size={20} color="#fafafa" weight="duotone" />
                 </div>
             </div>
             <div className="search-detail">
                 <div className="search-note">
                     <h3>{title}</h3>
-                    <div className="search-fund">
-                        <span>{amt} </span>
-                        <p>{info}</p>
-                    </div>
+                    <p><span>{amt} </span>{info}</p>
                 </div>
-                <div className="search-bar-min">
-                    <div className="urgent-bar"></div>
-                </div>
-                <div className="urgent-end">
+                <div className="bookmark-bar"></div>
+                <div className="bookmark-end">
                     <p><span>{num}</span> {txt}</p>
                     <p><span>{num2}</span> {txt2}</p>
                 </div>
