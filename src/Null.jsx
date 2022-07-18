b391d28d23a63eed8f87385b0921d48c70224036
collins <kevin1520smart@gmail.com>
2022-07-18 11:12:28 +0100

    correctly styled the search-null section

--- src/Null.jsx
+++ src/Null.jsx
@@ -1,6 +1,6 @@
 import React,{useState} from "react";
 import Menu from "./Menu";
-import {ArrowLeft, Bell,BookmarkSimple,DotsThreeVertical,SmileySad} from "phosphor-react";
+import {ArrowLeft, Bell,BookmarkSimple,DotsThreeVertical,FunnelSimple,MagnifyingGlass,SmileySad} from "phosphor-react";
 
 function Null({top,onBack,txt,onMain,book,search}){
     return(
@@ -8,13 +8,17 @@ function Null({top,onBack,txt,onMain,book,search}){
             {
                 search?
                 <div className="null">
-                    <div className="home-top">
-                        <div className="back">
-                            <ArrowLeft size={30} color="#1EBA60" onClick={onBack} />
-                            <h1>{top}</h1>
+                    <div className="back">
+                        <ArrowLeft size={30} color="#1EBA60" onClick={onBack} />
+                        <h1>{top}</h1>
+                    </div>
+                    <div className="search-max">
+                        <div className="search-min">
+                            <input type="" name="" placeholder="Search"/>
+                            <MagnifyingGlass size={20} color="#D3D3D3" weight="light" />
                         </div>
-                        <button className="dot" onClick={onMain}>
-                            <DotsThreeVertical size={30} color="#1EBA60" />
+                        <button className="dot">
+                            <FunnelSimple size={20} color="#1EBA60" weight="fill" />
                         </button>
                     </div>
                     <div className="bookmark-btn">
@@ -22,6 +26,10 @@ function Null({top,onBack,txt,onMain,book,search}){
                         <button>Art</button>
                         <button>Music</button>
                         <button>Sport</button>
+                    </div>
+                    <div className="search-txt">
+                        <p>Search Result</p>
+                        <p>0 found</p>
                     </div>
                     <div className="great">
                         <button>
