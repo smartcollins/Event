0f4e43a22e993ebb94e589e732b1da09cebf7443
collins <kevin1520smart@gmail.com>
2022-07-19 10:01:36 +0100

    modifying urgentItem section

--- src/UrgentItems.jsx
+++ src/UrgentItems.jsx
@@ -14,30 +14,32 @@ function UrgentItems(){
             {
                 back?
                 <Homepage/>:
-                <div className="Home">
-                    <div className="home-top">
-                        <div className="back">
-                            <ArrowLeft size={30} color="#1EBA60" onClick={onBack} />
-                            <h1>Urgent Fundraising</h1>
+                <div className="urgent-item">
+                    <div className="Home">
+                        <div className="home-top">
+                            <div className="back">
+                                <ArrowLeft size={30} color="#1EBA60" onClick={onBack} />
+                                <h1>Urgent Fundraising</h1>
+                            </div>
+                            <button className="dot">
+                                <DotsThreeVertical size={30} color="#1EBA60" />
+                            </button>
                         </div>
-                        <button className="dot">
-                            <DotsThreeVertical size={30} color="#1EBA60" />
-                        </button>
-                    </div>
-                    <div className="search-max">
-                        <div className="search-min">
-                            <input type="" name="" placeholder="Search"/>
-                            <MagnifyingGlass size={20} color="#D3D3D3" weight="light" />
+                        <div className="search-max">
+                            <div className="search-min">
+                                <input type="" name="" placeholder="Search"/>
+                                <MagnifyingGlass size={20} color="#D3D3D3" weight="light" />
+                            </div>
+                            <button className="dot">
+                                <FunnelSimple size={20} color="#1EBA60" weight="fill" />
+                            </button>
                         </div>
-                        <button className="dot">
-                            <FunnelSimple size={20} color="#1EBA60" weight="fill" />
-                        </button>
-                    </div>
-                    <div className="bookmark-btn">
-                        <button>All</button>
-                        <button>Art</button>
-                        <button>Music</button>
-                        <button>Sport</button>
+                        <div className="bookmark-btn">
+                            <button>All</button>
+                            <button>Art</button>
+                            <button>Music</button>
+                            <button>Sport</button>
+                        </div>
                     </div>
                 </div>
             }
