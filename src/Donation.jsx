b84f061869dfa02faa28a3748c21337f979149f1
collins <kevin1520smart@gmail.com>
2022-07-23 13:47:16 +0100

    Donation section in progress

--- src/Donation.jsx
+++ src/Donation.jsx
@@ -16,50 +16,46 @@ function Donation(){
 				<Homepage/> :
 				<div className="donation">
 					<div className="donation-img">
-						<div className="donation-top">
-							<ArrowLeft size={30} color="#1EBA60" weight="duotone" onClick={onBack}/>
-							<div className="donation-top-btn">
+						<div className="home-top">
+							<div className="back">
+								<ArrowLeft size={30} color="#1EBA60" weight="duotone" onClick={onBack}/>
+							</div>
+							<div className="home-top2">
 								<button>
-									<ShareNetwork size={30} color="#1EBA60" weight="light"/>
-									</button>
+									<ShareNetwork size={20} color="#13B157" weight="fill" />
+								</button>
 								<button>
-									<BookmarkSimple size={30} color="#1EBA60" weight="light"/>
+									<BookmarkSimple size={20} color="#13B157" weight="light" />
 								</button>
 							</div>
 						</div>
-						<div className="circle">
-							<Circle size={15} color="#1EBA60" weight="fill"/>
-							<Circle size={15} color="#FaFaFa" weight="duotone"/>
-							<Circle size={15} color="#FaFaFa" weight="duotone"/>
-							<Circle size={15} color="#FaFaFa" weight="duotone"/>
-							<Circle size={15} color="#FaFaFa" weight="duotone"/>
+						<div>
+							<Circle size={10} color="#1EBA60" weight="fill" />
+							<Circle size={10} color="#ffffff" weight="fill" />
+							<Circle size={10} color="#ffffff" weight="fill" />
+							<Circle size={10} color="#ffffff" weight="fill" />
 						</div>
 					</div>
-					<div className="donation-txt">
-						<div className="urgent-note">
-							<h3>Help Siamese Twins Surgery</h3>
-							<div className="donation-txt-top">
-								<p>$6,679 </p>
-								<p>	fund raised from $8,200</p>
-							</div>
-						</div>
-						<div className="urgent-bar"></div>
-						<div className="urgent-end">
+					<div className="bookmark-note">
+						<h3>Help Siamese Twins Surgrey</h3>
+						<p><span>$6,679 </span>fund raised from$8,200</p>
+						<div className="bookmark-bar"></div>
+						<div className="bookmark-end">
 							<p><span>3,438</span> Donators</p>
 							<p><span>11</span> days left</p>
 						</div>
-						<div className="donation-mid">
-							<button>Medical</button>
-							<div className="donation-mid-end">
-								<div className="donators"></div>
-								<div className="donators"></div>
-								<div className="donators"></div>
-								<p>3,438 donators</p>
-								<ArrowRight size={20} color="#1EBA60" weight="duotone"/>
-							</div>
-						</div>
-						<button className="nxt">Donate Now</button>
 					</div>
+					<div className="donation-mid">
+						<button>Medical</button>
+						<div className="donation-mid-end">
+							<div className="donators"></div>
+							<div className="donators"></div>
+							<div className="donators"></div>
+							<p>3,438 donators</p>
+							<ArrowRight size={20} color="#1EBA60" weight="duotone"/>
+						</div>
+					</div>
+					<button className="nxt">Donate Now</button>
 				</div>
 			}
 		</div>
