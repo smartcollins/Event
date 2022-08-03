d6880713acc1db491c8c32d6087fd6fdfbfda31d
collins <kevin1520smart@gmail.com>
2022-08-03 10:10:31 +0100

    styled fundraising top  correctly

--- src/Fundraising.jsx
+++ src/Fundraising.jsx
@@ -6,9 +6,14 @@ import {UsersThree,ArchiveBox,PlusCircle} from "phosphor-react"
 
 function Fundraising() {
     const [home,setHome] = useState(false);
+    const [top,setTop] = useState(false);
     
     function onHome(){
         setHome(true)
+    }
+
+    function onTop(){
+        setTop(true)
     }
 
     return(
@@ -16,18 +21,20 @@ function Fundraising() {
             {
                 home?
                 <Homepage/>:
-                <div className="Home">
+                <div className="fundraising">
                     <div className="home-top">
 						<div className="back">
 							<UsersThree size={40} color="#1EBA60" weight="duotone" onClick={onHome}/>
 							<h1>My Fundraising</h1>
 						</div>
-						<div className="home-top">
-                            <button className="dot">
-                                <ArchiveBox size={30} color="#1EBA60" weight="duotone"/>
-                            </button>
-                        </div>
+                        <button className="dot">
+                            <ArchiveBox size={30} color="#1EBA60" weight="duotone"/>
+                        </button>
 					</div>
+                    <div className="fundraising-top">
+                        <h3 onClick={onTop} className={!top ? "fundraising-top-click" : "fundraising-top-unclick"}>My Fundraising</h3>
+                        <h3 onClick={onTop} className={top ? "fundraising-top-click" : "fundraising-top-unclick"}>Activity</h3>
+                    </div>
                 </div>
             }
         </div>
