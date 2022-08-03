027fefe042a7c9c6d6af9788a7197e27b4f9ed21
collins <kevin1520smart@gmail.com>
2022-08-03 09:45:53 +0100

    added fundraising top

--- src/Fundraising.jsx
+++ src/Fundraising.jsx
@@ -1,5 +1,8 @@
 import React,{useState} from "react";
 import Homepage from "./Homepage";
+import Menu from "./Menu";
+import {UsersThree,ArchiveBox,PlusCircle} from "phosphor-react"
+
 
 function Fundraising() {
     const [home,setHome] = useState(false);
@@ -7,14 +10,24 @@ function Fundraising() {
     function onHome(){
         setHome(true)
     }
-    
+
     return(
         <div>
             {
                 home?
                 <Homepage/>:
-                <div>
-                    welcome
+                <div className="Home">
+                    <div className="home-top">
+						<div className="back">
+							<UsersThree size={40} color="#1EBA60" weight="duotone" onClick={onHome}/>
+							<h1>My Fundraising</h1>
+						</div>
+						<div className="home-top">
+                            <button className="dot">
+                                <ArchiveBox size={30} color="#1EBA60" weight="duotone"/>
+                            </button>
+                        </div>
+					</div>
                 </div>
             }
         </div>
