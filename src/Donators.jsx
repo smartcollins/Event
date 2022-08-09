9be190c30000ebcaca19220042a48df27680fef8
collins <kevin1520smart@gmail.com>
2022-08-09 18:10:02 +0100

    work in progress

--- src/Donators.jsx
+++ src/Donators.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import {SmileyWink,Smiley} from "phosphor-react";
 
-function Donators({img,name,anym}){
+function Donators({img,name,anym, thanks}){
     return(
         <div>
             {
                 anym?
-                <div className="donator-ayms">
-                    <div>
-                        <SmileyWink size={30} color="#ffffff" />
+                <div className={thanks&&"thanks"}>
+                    <div className="donator-ayms">
+                        <div>
+                            <SmileyWink size={30} color="#ffffff" />
+                        </div>
+                        <h3>Anonymous</h3>
                     </div>
-                    <h3>Anonymous</h3>
                 </div>:
-                <div className="donators">
-                    <div className={img}></div>
-                    <h3>{name}</h3>
+                <div className={thanks&&"thanks"}>
+                    <div className="donators">
+                        <div className={img}></div>
+                        <h3>{name}</h3>
+                    </div>
                 </div>
             }
         </div>
--- src/Fundraising.jsx
+++ src/Fundraising.jsx
@@ -5,6 +5,7 @@ import Menu from "./Menu";
 import SearchItem from "./SearchItem";
 import See from "./See";
 import Edit from "./Edit";
+import Donators from "./Donators";
 import {UsersThree,ArchiveBox,PlusCircle} from "phosphor-react"
 
 
@@ -12,7 +13,8 @@ import {UsersThree,ArchiveBox,PlusCircle} from "phosphor-react"
 function Fundraising() {
     const [home,setHome] = useState(false);
     const [calender,setCalender] = useState(false);
-    const [top,setTop] = useState(false);
+    const [top,setTop] = useState(true);
+    const [activity,setActivity] = useState(false)
     const [all,setAll] = useState(false);
     const [go, setGo] = useState(true);
     const [past, setPast] = useState(false);
@@ -34,7 +36,13 @@ function Fundraising() {
     }
 
     function onTop(){
-        setTop(oldTop => !top)
+        setTop(true)
+        setActivity(false)
+    }
+
+    function onActivity(){
+        setActivity(true)
+        setTop(false)
     }
 
     function onAll(){
@@ -72,94 +80,150 @@ function Fundraising() {
         setEdit(true)
     }
 
-    return(
-        <div>
-            {
-                edit?
-                <Edit/>:
-                see?
-                <See/>:
-                home?
-                <Homepage/>:
-                calender?
-                <Calender/>:
-                <div className="fundraising">
-                    <div className="home-top">
-						<div className="back">
-							<UsersThree size={40} color="#1EBA60" weight="duotone" onClick={onHome}/>
-							<h1>My Fundraising</h1>
-						</div>
-                        <button className="dot">
-                            <ArchiveBox size={30} color="#1EBA60" weight="duotone"/>
-                        </button>
-					</div>
-                    <div className="fundraising-top">
-                        <h3 onClick={onTop} className={!top ? "fundraising-top-click" : "fundraising-top-unclick"}>My Fundraising</h3>
-                        <h3 onClick={onTop} className={top ? "fundraising-top-click" : "fundraising-top-unclick"}>Activity</h3>
-                    </div>
-                    <PlusCircle className="plus" size={50} color="#1EBA60" weight="fill"/>
-                    <div className="fundraising-btn">
-                        <button onClick={onAll} className={all?"clicked":"unclicked"}>All(25)</button>
-                        <button onClick={onGo} className={go?"clicked":"unclicked"}>Ongoing(3)</button>
-                        <button onClick={onPast} className={past?"clicked":"unclicked"}>Past(22)</button>
-                        <button onClick={onPend} className={pend?"clicked":"unclicked"}>Pending(1)</button>
-                    </div>
-                    <div>
-                        {
-                            pend?
-                            <div className={pend&&"pend"}>
-                                <SearchItem
-                                    wait={true}
-                                    img="search-img2"
-                                    title="Help Improve Child Health"
-                                    amt="$2,775"
-                                    info="fund required"
-                                    num="21"
-                                    txt="Days left"
-                                />
-                            </div>:
-                            <div>
-                                <SearchItem
-                                    edit={true}
-                                    img="search-img8"
-                                    title="Help Victims of Earthquake"
-                                    amt="$2,275"
-                                    info="fund raised from $10,310"
-                                    num="2,475"
-                                    txt="Donators"
-                                    num2="21"
-                                    txt2="days left"
-                                    txt3= "$18"
-                                    onEdit={onEdit}
-                                    onSee={onSee}
-                                />
-                                <SearchItem
-                                    edit={true}
-                                    img="search-img2"
-                                    title="Help Improve Child Health"
-                                    amt="$2,775"
-                                    info="fund raised from $6,310"
-                                    num="938"
-                                    txt="Donators"
-                                    num2="21"
-                                    txt2="days left"
-                                    txt3= "$18"
-                                    onEdit={onEdit}
-                                    onSee={onSee}
-                                />
-                            </div>
-                        }
-                    </div>
-                    <Menu
-                        fund={true}
-                        onHome={onHome}
-                        onCalender={onCalender}
-                        onFund={onFund}
-                    />
+    return (
+      <div>
+        {edit ? (
+          <Edit />
+        ) : see ? (
+          <See />
+        ) : home ? (
+          <Homepage />
+        ) : calender ? (
+          <Calender />
+        ) : (
+          <div className="fundraising">
+            <div className="home-top">
+              <div className="back">
+                <UsersThree
+                  size={40}
+                  color="#1EBA60"
+                  weight="duotone"
+                  onClick={onHome}
+                />
+                <h1>My Fundraising</h1>
+              </div>
+              <button className="dot">
+                <ArchiveBox size={30} color="#1EBA60" weight="duotone" />
+              </button>
+            </div>
+            <div className="fundraising-top">
+              <h3
+                onClick={onTop}
+                className={
+                  top ? "fundraising-top-click" : "fundraising-top-unclick"
+                }
+              >
+                My Fundraising
+              </h3>
+              <h3
+                onClick={onActivity}
+                className={
+                  activity ? "fundraising-top-click" : "fundraising-top-unclick"
+                }
+              >
+                Activity
+              </h3>
+            </div>
+            <PlusCircle
+              className="plus"
+              size={50}
+              color="#1EBA60"
+              weight="fill"
+            />
+            {activity ? (
+              <div>
+                <Donators 
+                    thanks={true} 
+                    img="prayer-img4"
+                    name="jake Sparrow"
+                />
+              </div>
+            ) : (
+              <div>
+                <div className="fundraising-btn">
+                  <button
+                    onClick={onAll}
+                    className={all ? "clicked" : "unclicked"}
+                  >
+                    All(25)
+                  </button>
+                  <button
+                    onClick={onGo}
+                    className={go ? "clicked" : "unclicked"}
+                  >
+                    Ongoing(3)
+                  </button>
+                  <button
+                    onClick={onPast}
+                    className={past ? "clicked" : "unclicked"}
+                  >
+                    Past(22)
+                  </button>
+                  <button
+                    onClick={onPend}
+                    className={pend ? "clicked" : "unclicked"}
+                  >
+                    Pending(1)
+                  </button>
                 </div>
-            }
-        </div>
-    )
+                <div>
+                  {pend ? (
+                    <div className={pend && "pend"}>
+                      <SearchItem
+                        wait={true}
+                        img="search-img2"
+                        title="Help Improve Child Health"
+                        amt="$2,775"
+                        info="fund required"
+                        num="21"
+                        txt="Days left"
+                      />
+                    </div>
+                  ) : (
+                    <div>
+                      <SearchItem
+                        edit={true}
+                        img="search-img8"
+                        title="Help Victims of Earthquake"
+                        amt="$2,275"
+                        info="fund raised from $10,310"
+                        num="2,475"
+                        txt="Donators"
+                        num2="21"
+                        txt2="days left"
+                        txt3="$18"
+                        onEdit={onEdit}
+                        onSee={onSee}
+                      />
+                      <SearchItem
+                        edit={true}
+                        img="search-img2"
+                        title="Help Improve Child Health"
+                        amt="$2,775"
+                        info="fund raised from $6,310"
+                        num="938"
+                        txt="Donators"
+                        num2="21"
+                        txt2="days left"
+                        txt3="$18"
+                        onEdit={onEdit}
+                        onSee={onSee}
+                      />
+                    </div>
+                  )}
+                </div>
+              </div>
+            )}
+            <Menu
+              fund={true}
+              onHome={onHome}
+              onCalender={onCalender}
+              onFund={onFund}
+            />
+          </div>
+        )}
+      </div>
+    );
 }
 
 export default Fundraising;
