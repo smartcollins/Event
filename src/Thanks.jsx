93f1d488330e48525514ff09071c0e8176b2619b
collins <kevin1520smart@gmail.com>
2022-08-25 13:41:37 +0100

    created thanks section

---
+++ src/Thanks.jsx
@@ -0,0 +1,41 @@
+import React, {useState} from "react";
+import Edit from "./Edit";
+import Donators from "./Donators";
+import {ArrowLeft, DotsThreeVertical, PaperPlaneRight } from "phosphor-react";
+import { useState } from "react";
+
+function Thanks(){
+    const [back,setBack] = useState(false);
+    
+    function onBack(){
+        setBack(true)
+    }
+
+    return(
+        <div className="thank">
+            <div className="home-top">
+                <div className="back">
+                    <ArrowLeft size={30} color="#1EBA60" weight="duotone" onClick={onBack}/>
+                    <h1>Jane Cooper</h1>
+                </div>
+                <div className="home-top">
+                    <button className="dot">
+                        <DotsThreeVertical size={30} color="#1EBA60" weight="duotone"/>
+                    </button>
+                </div>
+            </div>
+            <button>Today</button>
+            <Donators 
+                thanks={true}
+                name= "Jane Cooper"
+                amt= "$27"
+            />
+            <div>
+                <p>Thank you very much Jane, God bless your kindness</p>
+                <p>09:41</p>
+            </div>
+        </div>
+    )
+}
+
+export default Thanks;
