5ecddc34f7b48657f8e9673eec4cef0136c50f88
collins <kevin1520smart@gmail.com>
2022-12-05 18:10:07 +0100

    cleaned up Donators

--- src/Donators.jsx
+++ src/Donators.jsx
@@ -1,38 +1,42 @@
 import React from "react";
-import {SmileyWink} from "phosphor-react";
+import { SmileyWink } from "phosphor-react";
 
-function Donators({img,name,anym, thanks, amt, onThanks, none}){
-    return (
-      <div>
-        {thanks ? (
-          <div className="thanks">
-            <div>
-              {thanks&&anym?
-                <SmileyWink className="ayms" size={30} color="#ffffff" />:
-                <div className={img}></div>
-              }
-              <p>
-                <b>{thanks&&anym?"Anonymous":name}</b> has donated <span>{amt}</span>
-              </p>
-            </div>
-            {none ? null : <button onClick={onThanks} className="unclicked">Say Thanks</button>}
-          </div>
-        ) : (
+function Donators({ img, name, anym, thanks, amt, onThanks, none }) {
+  return (
+    <div>
+      {thanks ? (
+        <div className="thanks">
           <div>
-            <div className="donators">
-              {anym?
-                <SmileyWink className="ayms" size={30} color="#ffffff" />:
-                <div className={img}></div>
-              }
-              {anym?
-                <h3>Anonoyms</h3>:
-                <h3>{name}</h3>
-              }
-            </div>
+            {thanks && anym ? (
+              <SmileyWink className="ayms" size={30} color="#ffffff" />
+            ) : (
+              <div className={img}></div>
+            )}
+            <p>
+              <b>{thanks && anym ? "Anonymous" : name}</b> has donated{" "}
+              <span>{amt}</span>
+            </p>
           </div>
-        )}
-      </div>
-    );
+          {none ? null : (
+            <button onClick={onThanks} className="unclicked">
+              Say Thanks
+            </button>
+          )}
+        </div>
+      ) : (
+        <div>
+          <div className="donators">
+            {anym ? (
+              <SmileyWink className="ayms" size={30} color="#ffffff" />
+            ) : (
+              <div className={img}></div>
+            )}
+            {anym ? <h3>Anonoyms</h3> : <h3>{name}</h3>}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Donators;
