fb7f3395570c1a0173c0cacd463dc31e2beef434
collins <kevin1520smart@gmail.com>
2022-12-05 18:13:23 +0100

    cleaned up Bookmarkitem

--- src/BookMarkItem.jsx
+++ src/BookMarkItem.jsx
@@ -1,27 +1,32 @@
 import React from "react";
-import {BookmarkSimple} from "phosphor-react";
+import { BookmarkSimple } from "phosphor-react";
 
-
-function BookMarkItem({img,title,amt,txt,num,num2,info,info2}){
-
-    return(
-        <div className="bookmark-card">
-            <div className={img}>
-                <div className="bookmark-mark">
-                    <BookmarkSimple size={20} color="#ffffff" weight="fill" />
-                </div>
-            </div>
-            <div className="bookmark-note">
-                <h3>{title}</h3>
-                <p><span>{amt} </span>{txt} </p>
-                <div className="bookmark-bar"></div>
-                <div className="bookmark-end">
-                    <p><span>{num}</span> {info}</p>
-                    <p><span>{num2}</span> {info2}</p>
-                </div>
-            </div>
+function BookMarkItem({ img, title, amt, txt, num, num2, info, info2 }) {
+  return (
+    <div className="bookmark-card">
+      <div className={img}>
+        <div className="bookmark-mark">
+          <BookmarkSimple size={20} color="#ffffff" weight="fill" />
         </div>
-    )
+      </div>
+      <div className="bookmark-note">
+        <h3>{title}</h3>
+        <p>
+          <span>{amt} </span>
+          {txt}{" "}
+        </p>
+        <div className="bookmark-bar"></div>
+        <div className="bookmark-end">
+          <p>
+            <span>{num}</span> {info}
+          </p>
+          <p>
+            <span>{num2}</span> {info2}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default BookMarkItem;
