fb8af4e4aca311df8f9784423035a309cf2d66b5
collins <kevin1520smart@gmail.com>
2022-12-06 18:25:53 +0100

    cleaned up Menu

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,68 +1,76 @@
 import React from "react";
-import {House,Notepad,ListDashes,ChatText,User} from "phosphor-react";
+import { House, Notepad, ListDashes, ChatText, User } from "phosphor-react";
 
-function Menu({ home, onHome, calender, onCalender, fund, onFund, inbox, onInbox}){
+function Menu({
+  home,
+  onHome,
+  calender,
+  onCalender,
+  fund,
+  onFund,
+  inbox,
+  onInbox,
+}) {
+  function onHouse() {
+    onHome();
+  }
 
-    function onHouse(){
-        onHome()
-    }
+  function onDonation() {
+    onCalender();
+  }
 
-    function onDonation(){
-        onCalender()
-    }
+  function onFundraising() {
+    onFund();
+  }
 
-    function onFundraising(){
-        onFund()
-    }
+  function onMessages() {
+    onInbox();
+  }
 
-    function onMessages(){
-        onInbox()
-    }
-	
-	return(
-        <div className="menu">
-            {
-                home?
-                <button onClick={onHouse} className="menu-clicked" >
-                    <House size={30} color="#ffffff"/>
-                </button>:
-                <button onClick={onHouse} className="menu-unclicked" >
-                    <House size={30} color="#1EBA60"/>
-                </button>
-            }
-            {
-                calender?
-                <button onClick={onDonation} className="menu-clicked" >
-                    <Notepad size={30} color="#ffffff"/>
-                </button>:
-                <button onClick={onDonation} className="menu-unclicked" >
-                    <Notepad size={30} color="#1EBA60"/>
-                </button>
-            }
-            {
-                fund?
-                <button onClick={onFundraising} className="menu-clicked" >
-                    <ListDashes size={30} color="#ffffff"/>
-                </button>:
-                <button onClick={onFundraising} className="menu-unclicked" >
-                    <ListDashes size={30} color="#1EBA60"/>
-                </button>
-            }
-            {
-                inbox?
-                <button onClick={onMessages} className="menu-clicked" >
-                    <ChatText size={30} color="#ffffff"/>
-                </button>:
-                <button onClick={onMessages} className="menu-unclicked" >
-                    <ChatText size={30} color="#1EBA60"/>
-                </button>
-            }
+  return (
+    <div className="menu">
+      {home ? (
+        <button onClick={onHouse} className="menu-clicked">
+          <House size={30} color="#ffffff" />
+        </button>
+      ) : (
+        <button onClick={onHouse} className="menu-unclicked">
+          <House size={30} color="#1EBA60" />
+        </button>
+      )}
+      {calender ? (
+        <button onClick={onDonation} className="menu-clicked">
+          <Notepad size={30} color="#ffffff" />
+        </button>
+      ) : (
+        <button onClick={onDonation} className="menu-unclicked">
+          <Notepad size={30} color="#1EBA60" />
+        </button>
+      )}
+      {fund ? (
+        <button onClick={onFundraising} className="menu-clicked">
+          <ListDashes size={30} color="#ffffff" />
+        </button>
+      ) : (
+        <button onClick={onFundraising} className="menu-unclicked">
+          <ListDashes size={30} color="#1EBA60" />
+        </button>
+      )}
+      {inbox ? (
+        <button onClick={onMessages} className="menu-clicked">
+          <ChatText size={30} color="#ffffff" />
+        </button>
+      ) : (
+        <button onClick={onMessages} className="menu-unclicked">
+          <ChatText size={30} color="#1EBA60" />
+        </button>
+      )}
 
-            <button>
-            <User size={30} color="#1EBA60" />
-            </button>
-        </div>
-    )
+      <button>
+        <User size={30} color="#1EBA60" />
+      </button>
+    </div>
+  );
 }
 
 export default Menu;
