bc7724951aec65f00182f87487947f2aed9eb9cf
collins <kevin1520smart@gmail.com>
2023-04-04 23:24:23 +0100

    formatted ComingItems

--- src/ComingItems.jsx
+++ src/ComingItems.jsx
@@ -15,54 +15,54 @@ function ComingItems() {
     setBack(true);
   }
 
-  const itemsAry =[
+  const itemsAry = [
     {
-               img:"search-img",
-              title:"Help Overcome Malnutrition",
-              amt:"$8,775",
-              info:"fund raised from $10,540",
-              num:"4,471",
-              txt:"Donators",
-              num2:"2",
-              txt2:"days left"
+      img: "search-img",
+      title: "Help Overcome Malnutrition",
+      amt: "$8,775",
+      info: "fund raised from $10,540",
+      num: "4,471",
+      txt: "Donators",
+      num2: "2",
+      txt2: "days left"
     },
-     
-            {
-               img:"search-img6",
-              title:"Help Improve Child Health",
-              amt:"$2,777",
-              info:"fund raised from $6,310",
-              num:"4,376",
-              txt:"Donators",
-              num2:"4",
-              txt2:"days left"
-            },
-             
-            {
-               img:"search-img5",
-              title:"Help Victims of Flooding",
-              amt:"$7,475",
-              info:"fund raised from $8,380",
-              num:"2,118",
-              txt:"Donators",
-              num2:"3",
-              txt2:"days left"
-            },
-             
-            {
-               img:"search-img7",
-              title:"Help Improve Healthy Food For Children",
-              amt:"$9,789",
-              info:"fund raised from $10,340",
-              num:"5,348",
-              txt:"Donators",
-              num2:"4",
-              txt2:"days left"
-            }
-             
+
+    {
+      img: "search-img6",
+      title: "Help Improve Child Health",
+      amt: "$2,777",
+      info: "fund raised from $6,310",
+      num: "4,376",
+      txt: "Donators",
+      num2: "4",
+      txt2: "days left"
+    },
+
+    {
+      img: "search-img5",
+      title: "Help Victims of Flooding",
+      amt: "$7,475",
+      info: "fund raised from $8,380",
+      num: "2,118",
+      txt: "Donators",
+      num2: "3",
+      txt2: "days left"
+    },
+
+    {
+      img: "search-img7",
+      title: "Help Improve Healthy Food For Children",
+      amt: "$9,789",
+      info: "fund raised from $10,340",
+      num: "5,348",
+      txt: "Donators",
+      num2: "4",
+      txt2: "days left"
+    }
+
   ]
 
-  const items = itemsAry.map((item,idx)=><SearchItem key={idx} {...item}/>)
+  const items = itemsAry.map((item, idx) => <SearchItem key={idx} {...item} />)
 
   return (
     <div>
