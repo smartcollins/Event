59a7d0a732b68b074b27faf5db3eca377b60bf7d
collins <kevin1520smart@gmail.com>
2023-04-06 22:50:14 +0100

    modified Pin

--- src/Pin.jsx
+++ src/Pin.jsx
@@ -2,6 +2,18 @@ import React from "react";
 import { ArrowLeft, Circle } from "phosphor-react";
 
 function Pin({ onCreateBack, top, txt, txt2, btn, onCreatePin }) {
+
+  const circleAry = [
+     {size:16, color:"#1EBA60", weight:"fill"},
+           {size:16, color:"#1EBA60", weight:"fill"},
+           {size:16, color:"#1EBA60", weight:"fill"},
+           {size:16, color:"#1EBA60", weight:"fill"},
+           {size:16, color:"#E6E6E6", weight:"fill"},
+           {size:16, color:"#E6E6E6", weight:"fill"}
+  ]
+
+  const circle = circleAry.map((item,idx)=><Circle key={idx} {...item}/>)
+
   return (
     <div className="Pin">
       <div className="back">
@@ -17,12 +29,13 @@ function Pin({ onCreateBack, top, txt, txt2, btn, onCreatePin }) {
       <div className="create">
         <h2>{txt2}</h2>
         <div className="pin">
-          <Circle size={16} color="#1EBA60" weight="fill" />
+          {/* <Circle size={16} color="#1EBA60" weight="fill" />
           <Circle size={16} color="#1EBA60" weight="fill" />
           <Circle size={16} color="#1EBA60" weight="fill" />
           <Circle size={16} color="#1EBA60" weight="fill" />
           <Circle size={16} color="#E6E6E6" weight="fill" />
-          <Circle size={16} color="#E6E6E6" weight="fill" />
+          <Circle size={16} color="#E6E6E6" weight="fill" /> */}
+          {circle}
         </div>
         <button className="nxt" onClick={onCreatePin}>
           {btn}
