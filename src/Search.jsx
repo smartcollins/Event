3c90e753ffb8864a65f8f415da4487577e0db2fc
collins <kevin1520smart@gmail.com>
2023-04-08 19:51:14 +0100

    formatted Search

--- src/Search.jsx
+++ src/Search.jsx
@@ -11,37 +11,43 @@ function Search() {
   }
 
   const searchAry = [
-    
-            {img:"search-img",
-            title:"Help Little Baby Surgery",
-            amt:"$2,275",
-            info:"fund raised from $10,310",
-            num:"4,471",
-            txt:"Donators",
-            num2:"9",
-            txt2:"days left"},
-          
-            {img:"search-img2",
-            title:"Help Overcome Malnutrition",
-            amt:"$8, 775",
-            info:"fund raised from $7,310",
-            num:"2,471",
-            txt:"Donators",
-            num2:"21",
-            txt2:"days left"},
-          
-            {img:"search-img3",
-            title:"Help Little Baby Surgery",
-            amt:"$2,205",
-            info:"fund raised from $5,310",
-            num:"1,171",
-            txt:"Donators",
-            num2:"5",
-            txt2:"days left"},
+
+    {
+      img: "search-img",
+      title: "Help Little Baby Surgery",
+      amt: "$2,275",
+      info: "fund raised from $10,310",
+      num: "4,471",
+      txt: "Donators",
+      num2: "9",
+      txt2: "days left"
+    },
+
+    {
+      img: "search-img2",
+      title: "Help Overcome Malnutrition",
+      amt: "$8, 775",
+      info: "fund raised from $7,310",
+      num: "2,471",
+      txt: "Donators",
+      num2: "21",
+      txt2: "days left"
+    },
+
+    {
+      img: "search-img3",
+      title: "Help Little Baby Surgery",
+      amt: "$2,205",
+      info: "fund raised from $5,310",
+      num: "1,171",
+      txt: "Donators",
+      num2: "5",
+      txt2: "days left"
+    },
 
   ]
 
-  const items = searchAry.map((item,idx)=><SearchItem key={idx} {...item}/>)
+  const items = searchAry.map((item, idx) => <SearchItem key={idx} {...item} />)
 
   return (
     <div>
