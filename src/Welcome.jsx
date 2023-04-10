d024820a87b0d28e772c1ffc61c44b6e5be38b00
collins <kevin1520smart@gmail.com>
2023-04-10 23:10:42 +0100

    created array in Welcome

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -28,6 +28,26 @@ function Welcome() {
     setForgotPass(true);
   }
 
+  const signAry = [
+    
+    {txt:"Sign up for free",
+    txt2:"Sign Up",
+    txt3:"Already have an account",
+    onSign:onSign,
+    txt4:"Sign In",
+    onSwitch:onSignIn,
+  },
+  {  txt:"Sign in to your account",
+    txt2:"Sign In",
+    txt3:"Don't have an Account",
+    txt4:"Sign Up",
+    forgot:"Forgot the Password?",
+    onSign:onSign,
+    onForgot:onForgot,
+    onSwitch:onSignUp},
+
+  ]
+
   return (
     <div>
       {sign ? (
