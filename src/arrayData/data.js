f83026e52fe936b53cbc6f686f089c81f64d364b
collins <kevin1520smart@gmail.com>
2023-04-12 23:27:44 +0100

    created data.js

---
+++ src/arrayData/data.js
@@ -0,0 +1,22 @@
+export default signAry = [
+
+    {
+      txt: "Sign up for free",
+      txt2: "Sign Up",
+      txt3: "Already have an account",
+      onSign: { onSign },
+      txt4: "Sign In",
+      onSwitch: onSignIn,
+    },
+    {
+      txt: "Sign in to your account",
+      txt2: "Sign In",
+      txt3: "Don't have an Account",
+      txt4: "Sign Up",
+      forgot: "Forgot the Password?",
+      onSign: { onSign },
+      onForgot: { onForgot },
+      onSwitch: { onSignUp }
+    },
+
+  ]
