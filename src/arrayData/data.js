806b7633db1c2cdb6bb8e0bfb163b1f49b94f0c9
collins <kevin1520smart@gmail.com>
2023-04-12 23:49:32 +0100

    added aray for ComingItem in data

--- src/arrayData/data.js
+++ src/arrayData/data.js
@@ -1,3 +1,4 @@
+// in Bookmark.jsx
 export  const bookmarks = [{
     img: "bookmark-img",
     title: "Help Dilan Brain Turmor Surgery",
@@ -18,7 +19,53 @@ export  const bookmarks = [{
     info2: "days left",
   }]
 
+//in ComingItem.jsx
+export const itemsAry = [
+    {
+      img: "search-img",
+      title: "Help Overcome Malnutrition",
+      amt: "$8,775",
+      info: "fund raised from $10,540",
+      num: "4,471",
+      txt: "Donators",
+      num2: "2",
+      txt2: "days left"
+    },
 
+    {
+      img: "search-img6",
+      title: "Help Improve Child Health",
+      amt: "$2,777",
+      info: "fund raised from $6,310",
+      num: "4,376",
+      txt: "Donators",
+      num2: "4",
+      txt2: "days left"
+    },
+
+    {
+      img: "search-img5",
+      title: "Help Victims of Flooding",
+      amt: "$7,475",
+      info: "fund raised from $8,380",
+      num: "2,118",
+      txt: "Donators",
+      num2: "3",
+      txt2: "days left"
+    },
+
+    {
+      img: "search-img7",
+      title: "Help Improve Healthy Food For Children",
+      amt: "$9,789",
+      info: "fund raised from $10,340",
+      num: "5,348",
+      txt: "Donators",
+      num2: "4",
+      txt2: "days left"
+    }
+
+  ]
 
 
 export const signAry = [
